3f5a6fa8c7fc183af90295a095b994d06ff1e962
biggjuann79 <jungbrothers@gmail.com>
2025-06-29 15:29:15 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ def read_root():
 return {"message": "Hello World"}
 if __name__ == "__main__":
 import uvicorn import os
-port = intos. getenv ("PORT", 8000) )
+port = intos. getenv ("PORT", 8000) 
 uvicorn. run(app, host="0.0.0.0", port=port)
