cc8e43916a8349f0b783e59153a0ada2041a08cb
biggjuann79 <jungbrothers@gmail.com>
2025-06-29 14:56:31 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+app = FastAPI(title="Test API")
+@app. get ("/")
+def read_root():
+return {"message": "Hello World"}
+if __name__ == "__main__":
+import uvicorn import os
+port = intos. getenv ("PORT", 8000) )
+uvicorn. run(app, host="0.0.0.0", port=port)
